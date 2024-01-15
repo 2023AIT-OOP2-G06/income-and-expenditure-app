0c62b00b4085b01c543710f64f4624a1eb48d03d
ASRockI <k22110kk@aitech.ac.jp>
2024-01-15 21:59:05 +0900

    Screen add

---
+++ docs/features/incomeAndOutgo/js/incomeAndOutgoMain.js
@@ -0,0 +1,12 @@
+// 収入管理ページ用のカスタムJavaScript
+
+// Sample data for the income chart
+var incomeData = {
+    labels: ['食費', '光熱費', 'その他'],
+    datasets: [{
+        label: '扶養の金額',
+        data: [30000, 15000, 20000],
+        backgroundColor: ['#3498db', '#e74c3c', '#2ecc71'],
+        borderWidth: 1
+    }]
+};
