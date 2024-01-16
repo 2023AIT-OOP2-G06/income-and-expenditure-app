a9a11e70d4a40cad02f7c18857e8508a8d8c9d53
ASRockI <k22110kk@aitech.ac.jp>
2024-01-16 18:07:44 +0900

    variable  add

--- docs/features/incomeAndOutgo/js/incomeAndOutgoMain.js
+++ docs/features/incomeAndOutgo/js/incomeAndOutgoMain.js
@@ -1,12 +1,47 @@
 // 収入管理ページ用のカスタムJavaScript
 
-// Sample data for the income chart
-var incomeData = {
-    labels: ['食費', '光熱費', 'その他'],
-    datasets: [{
-        label: '扶養の金額',
-        data: [30000, 15000, 20000],
-        backgroundColor: ['#3498db', '#e74c3c', '#2ecc71'],
-        borderWidth: 1
-    }]
-};
+//----ー扶養欄の働いた金額----ー
+var fuyo_worked = 600000;
+// HTMLに計算結果を挿入
+document.getElementById('fuyo_work_value').innerHTML = '働いた金額 : ' + fuyo_worked.toLocaleString() + '円';
+
+//----ー扶養欄の残り金額----ー
+var fuyo_value = 1030000;
+var fuyo_rest=fuyo_value - fuyo_worked;
+// HTMLに計算結果を挿入
+document.getElementById('fuyo_rest_value').innerHTML = '残った金額 : ' + fuyo_rest.toLocaleString() + '円';
+
+
+//----ー収入欄の働いた金額----ー
+var in_worked = 60000;
+// HTMLに計算結果を挿入
+document.getElementById('in_work_value').innerHTML = '働いた金額 : ' + in_worked.toLocaleString() + '円';
+
+//----ー収入欄の勤務時間----ー
+var in_time = 60;
+// HTMLに計算結果を挿入
+document.getElementById('in_work_time').innerHTML = '勤務時間 : ' + in_time + '時間';
+
+
+//----ー支出欄----ー
+var out_value1 = 60000;
+var out_value2 = 10000;
+var total_out = out_value1 + out_value2;
+// HTMLに計算結果を挿入
+document.getElementById('out_value').innerHTML = '支出の合計 : ' + total_out.toLocaleString() + '円';
+
+
+//----ー収支欄----ー
+var total_bop = in_worked - total_out;
+// HTMLに計算結果を挿入
+document.getElementById('bop_value').innerHTML = '収支の合計 : ' + total_bop.toLocaleString() + '円';
+
+
+
+//ー----グラフのパーセンテージ----ー
+var percentage = (fuyo_rest / fuyo_value)*100; // 例として100とします
+// CSS変数に値をセット
+document.documentElement.style.setProperty('--my-calculated-value', percentage + '%');
+
+
+
