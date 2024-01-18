5b466ad046e721ae23394268c075a6997f20e1ee
k22133 <k22133kk@aitech.ac.jp>
2024-01-18 15:55:19 +0900

    画面表示機能の完成

--- docs/features/outgo/js/OutgoMain.js
+++ docs/features/outgo/js/OutgoMain.js
@@ -1,8 +1,33 @@
-class OutgoMain {
+class OutgoMain { 
     getOutgo(){
         const outgoRepository = new OutgoRepository();
         const outgo = outgoRepository.getOutgoList();
         console.log(outgo);
+        
+        // 画面上に表示
+        const ulPointer = document.querySelector('ul');
+        while(ulPointer.firstChild){// 一旦全消去
+            ulPointer.removeChild(ulPointer.firstChild);
+        }
+        outgo.forEach(element => {
+            const newLi = document.createElement('li');
+            newLi.classList.add('list-group-item');
+            const newDate = document.createElement('div');
+            // dateの表示を年月日に限定
+            const spStr = element.date.split('T');
+            newDate.innerText = spStr[0];
+            newLi.appendChild(newDate);
+            const newPrice = document.createElement('div');
+            newPrice.innerText = element.price;
+            newLi.appendChild(newPrice);
+            /*
+            <li class='list-group-item'>
+                <div>spStr[0]</div>
+                <div>element.price</div>
+            </li>
+             */
+            ulPointer.appendChild(newLi);
+        });
     };
 
     setOutgo(){
@@ -24,4 +49,9 @@ class OutgoMain {
         const outgoMonthAll = outgoRepository.getOutgoMonthAll(2024, 1);
         console.log(outgoMonthAll);
     }
+}
+
+window.onload = function(){
+    const OutgoMain = new OutgoMain();
+    OutgoMain.getOutgo()
 }
