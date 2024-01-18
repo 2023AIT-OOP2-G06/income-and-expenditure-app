b0a134f9a50f4f13c2b015070ec0a8928a4196d5
nanairo2743 <136669608+nanairo2743@users.noreply.github.com>
2024-01-18 13:28:41 +0900

    JobDateのデータベースの完成
    jobDateの制作

--- docs/DB/jobData/JobApi.ts
+++ docs/DB/jobData/JobApi.ts
@@ -1,1 +1,22 @@
 //TODO:中身を作成する
+class JobApi {
+    private static key: string = 'JobList';
+
+    // @param {JobModel[]} JobList
+    static setJob(
+        job: JobModel
+    ) {
+
+        const JobString = JSON.stringify(job);
+
+        localStorage.setItem(this.key, JobString);
+    }
+
+    static getJob(){
+        return localStorage.getItem(this.key);
+    }
+
+    static deleteJob(){
+        localStorage.removeItem(this.key);
+    }
+}
--- docs/DB/jobData/JobEntity.ts
+++ docs/DB/jobData/JobEntity.ts
@@ -1,1 +1,6 @@
 //TODO:中身を作成する
+type JobModel = {
+    jobname: string;
+    date: Date;
+    price: number;
+}
