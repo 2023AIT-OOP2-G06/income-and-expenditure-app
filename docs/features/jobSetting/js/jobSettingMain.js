d1f040cba5ff7578677c936629856267a9fcdcb1
teratanitaiki <153792045+teratanitaiki@users.noreply.github.com>
2024-01-25 15:04:05 +0900

    数字の範囲指定　アラート

--- docs/features/jobSetting/js/jobSettingMain.js
+++ docs/features/jobSetting/js/jobSettingMain.js
@@ -16,20 +16,28 @@ class JobSettingMain {
         const valName = inputname.value;
         const valDate = inputDate.value;
         const valPrice = inputPrice.value;
-        inputname.value = '';
-        inputDate.value = '';
-        inputPrice.value = '';
 
-        const job = {
-            jobname:String(valName),
-            // 来月に変更する
-            payday:Number(valDate),
-            price:Number(valPrice),
-        };
-        
-        console.log(job);
+        try{
+            if(valDate < 1 || valDate > 31){
+                throw new Error('日付が不適切です');
+            }
+            const job = {
+                jobname:String(valName),
+                // 来月に変更する
+                payday:Number(valDate),
+                price:Number(valPrice),
+            };
+            
+            console.log(job);
+    
+            jobRepository.setJob(job);
+    
+            alert('保存完了');
 
-        jobRepository.setJob(job);
+        }catch(e){
+            console.log(e.name,e.message);
+            alert(e.message);
+        }
 
     }
     
