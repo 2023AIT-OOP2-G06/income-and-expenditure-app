5f59f19b9b6f87737826fb512096ef7b72c658d8
teratanitaiki <153792045+teratanitaiki@users.noreply.github.com>
2024-01-25 14:09:18 +0900

    完成　装飾のみ

--- docs/features/jobSetting/js/jobSettingMain.js
+++ docs/features/jobSetting/js/jobSettingMain.js
@@ -9,11 +9,22 @@ class JobSettingMain {
 
         const jobRepository = new JobRepository();
 
+        // 取得
+        const inputname = document.querySelector('#new-name');
+        const inputDate = document.querySelector('#new-date');
+        const inputPrice = document.querySelector('#new-price');
+        const valName = inputname.value;
+        const valDate = inputDate.value;
+        const valPrice = inputPrice.value;
+        inputname.value = '';
+        inputDate.value = '';
+        inputPrice.value = '';
+
         const job = {
-            jobname: 'ファミリーマート',
+            jobname:String(valName),
             // 来月に変更する
-            payday: 15,
-            price: 1000,
+            payday:Number(valDate),
+            price:Number(valPrice),
         };
         
         console.log(job);
